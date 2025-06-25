f4a8df780629d975b13eaad036be544fdfbca319
paihangsan <59058401+paihangsan@users.noreply.github.com>
2025-06-25 19:22:24 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -147,7 +147,6 @@ const API_SITES = {
      lsb: {
          api: 'https://apilsbzy1.com',
          name: 'lsb资源',
-         adult: true
      },
      huangcang: {
          api: 'https://hsckzy.vip',
